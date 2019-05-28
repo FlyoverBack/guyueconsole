7eac054a33c92a13b7f29e90d822b5d9c9a956ad
ITOL <1104026790@qq.com>
2019-05-28 11:50:37 +0800

    修改nginx ip映射关系

--- src/main/java/com/bootdo/common/utils/IPUtils.java
+++ src/main/java/com/bootdo/common/utils/IPUtils.java
@@ -16,7 +16,7 @@ public class IPUtils {
 	 */
 	public static String getIpAddr(HttpServletRequest request) {
 
-		String ip = request.getHeader("x-forwarded-for");
+		String ip = request.getHeader("X-real-ip");
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
