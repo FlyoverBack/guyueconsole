3aab86ff48ce2cdd72eed7cc6eec493be65ff605
liujie <759807663@qq.com>
2019-07-20 11:27:24 +0800

    尝试样式删除

--- src/main/resources/static/js/appjs/club/club/edit.js
+++ src/main/resources/static/js/appjs/club/club/edit.js
@@ -1,12 +1,6 @@
 $().ready(function() {
 	validateRule();
 });
-$(function(){
-    $("#loginform").submit(function(e){
-        return false;
-    });
-    $("#modaltrigger").leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
-});
 $.validator.setDefaults({
 	submitHandler : function() {
 		update();
